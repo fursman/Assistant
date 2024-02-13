f71effaa85f2d5afa6f6004941a71fc6307dbb89
fursman <120282785+fursman@users.noreply.github.com>
2024-02-12 17:51:44 -0800

    Add files via upload

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? (import <nixpkgs> { 
+    config.allowUnfree = true;
+    config.segger-jlink.acceptLicense = true; 
+  })
+}:
+
+(pkgs.mkShell.override { stdenv = pkgs.gcc11Stdenv; }) {
+  buildInputs = [
+    (pkgs.python3.withPackages (ps: [ ps.numpy ps.pyaudio ps.notify2 ps.keyring ]))
+    pkgs.virtualenv
+    pkgs.portaudio
+    pkgs.ffmpeg
+  ];
+  shellHook = ''
+    DIR="${builtins.toPath ./.}"
+    VENV_DIR="$DIR/venv"
+    SOURCE_DATE_EPOCH=$(date +%s) # required for python wheels
+    virtualenv --no-setuptools "$VENV_DIR"
+    export PYTHONPATH="$VENV_DIR/${pkgs.python3.sitePackages}:$PYTHONPATH"
+    export PATH="$VENV_DIR/bin:$PATH"
+
+    source venv/bin/activate
+  '';
+}
